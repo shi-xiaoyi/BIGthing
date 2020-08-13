903ddc841e1bdf7397e52bf35ef3f5c23c0acfbc
shi-xiaoyi <1836585880@qq.com>
2020-08-13 18:40:31 +0800

    用户资料初步

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,49 +2,12 @@ $(function () {
 
     getuse();
     // 获取用户的基本信息
-    function getuse() {
 
-        $.ajax({
-            method: 'GET',
-            url: '/my/userinfo',
-            headers: {
-                Authorization: localStorage.getItem('token')
-            },
-            success: function (res) {
-                // console.log(res);
-                if (res.status !== 0) {
-                    return
-                }
-                // console.log(res);
-                getimage(res.data);
-            }
-        })
-    }
     //渲染用户的头像和文字信息
 
-    function getimage(data) {
-        // 获取需要的用户名
 
-        var name = data.nickname || data.username;
-        $('#welcome').html(`欢迎  ${name}`)
-
-        //渲染那个图片
-        if (!data.user_pic) {
-            // 渲染文本图片
-            $('.layui-nav-img').hide();
-            var first_name = name[0].toUpperCase()
-            $('.text-avatar').html(first_name).show();
-            $('.a-text').html(first_name).show();
-        } else {
-            $('.layui-nav-img')
-                .attr('src', user.user_pic)
-                .show()
-            $('.text-avatar').hide()
-            $('.a-text').hide()
-        }
-    }
     // 在页面加在后清空本地
-    localStorage.removeItem('token');
+    // localStorage.removeItem('token');
 
     //点击退出按钮
     var layer = layui.layer
@@ -68,3 +31,44 @@ $(function () {
     })
 
 })
+
+function getuse() {
+
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token')
+        },
+        success: function (res) {
+            // console.log(res);
+            if (res.status !== 0) {
+                return
+            }
+            // console.log(res);
+            getimage(res.data);
+        }
+    })
+}
+
+function getimage(data) {
+    // 获取需要的用户名
+
+    var name = data.nickname || data.username;
+    $('#welcome').html(`欢迎  ${name}`)
+
+    //渲染那个图片
+    if (!data.user_pic) {
+        // 渲染文本图片
+        $('.layui-nav-img').hide();
+        var first_name = name[0].toUpperCase()
+        $('.text-avatar').html(first_name).show();
+        $('.a-text').html(first_name).show();
+    } else {
+        $('.layui-nav-img')
+            .attr('src', user.user_pic)
+            .show()
+        $('.text-avatar').hide()
+        $('.a-text').hide()
+    }
+}
